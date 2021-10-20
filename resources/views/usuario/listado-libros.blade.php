867c37a28756da71fe59239f9de874773fb62e8a
Hugob79 <hugoboulocq@gmail.com>
2021-10-19 21:01:52 -0300

    Corrijo acentos en la sección del usuario

--- resources/views/usuario/listado-libros.blade.php
+++ resources/views/usuario/listado-libros.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 @section('contenido_app')
 <div class="container mt-4">
-    <h1>Estas en la seccion del usuario</h1>
+    <h1>Estás en la sección del usuario</h1>
     <a href="/ingresarLibro"><i class="fas fa-pen-alt"></i> Ingresar un libro nuevo</a>
 
     <table class="table table-info table-bordered table-hover" id="tabla_resultados">
         <thead>
             <tr>
-                <th>Titulo</th>
-                <th>Genero</th>
-                <th>Descripcion</th>
+                <th>Título</th>
+                <th>Género</th>
+                <th>Descripción</th>
                 <td>Autor</td>
                 <th>Tapa</th>
                 <th>Descargar</th>
@@ -26,8 +26,8 @@
                 <td>{{$libro->autor}}</td>
                 <td><img src="{{$libro->imagen}}" width="200px" alt="Libro"></td>
                 <td><a href="{{$libro->enlace_libro}}"></a><button class="btn btn-danger">Descargar</button></td>
-                <td><button>Modifica</button></td>
-                <td>Elimina</td>
+                <td><button>Modifica (próximamente)</button></td>
+                <td>Elimina (próximamente)</td>
             </tr>
             @endforeach
         </tbody>
