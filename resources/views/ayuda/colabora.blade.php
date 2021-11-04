43ed7411d7b2c4b96f7ba543ca24c59a852c8c28
Hugob79 <hugoboulocq@gmail.com>
2021-11-03 21:32:54 -0300

    Agrego imagen pintada a mano del titulo colaborá. Modifico el texto de esa sección basándome en las 4 libertades del Software Libre

--- resources/views/ayuda/colabora.blade.php
+++ resources/views/ayuda/colabora.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')
 @section('contenido_app')
 <div class="container mt-4">
-    <h1>Colaborá</h1>
-    <p>Este sitio se encuentra realizado en Laravel y liberado todo su código en GitHub.</p> 
-    <p>
-    Para clonarlo o realizar mejoras, simplemente <a href="https://github.com/Hugob79/biblioteca-enlace-libre.git" target="_blank">clonalo</a> </p>
-        <p>Si en cambio, querés colaborar en este proyecto, <a href="mailto:enlacepilar@gmail.com">escribime.</a></p>
+    <img src="/imagenes/colabora02.png" class="img-fluid" alt="Colaborá">
+    <div class="alert-warning p-4">
+        <p>Este sitio se encuentra realizado en Laravel y liberado todo su código en GitHub.</p> 
+        <p> Para clonarlo o realizar mejoras, hacé clic <a href="https://github.com/Hugob79/biblioteca-enlace-libre.git" target="_blank">acá</a> </p>
+        <p>Sos libre de (basado en las 4 libertades del Software Libre):</p>
+        <p>-Clonar el repositorio y usarlo con cualquier propósito.
+        <p>-Poder estudiar el código y modificarlo.</p>
+        <p>-¡Poder compartir el repositorio o avisarle a alguien más!</p>
+        <p>-Poder mejorar el sitio y poder compartir dichas mejoras para beneficio de todos.</p>
+        <br>
+        <hr>
+        <p>Si querés colaborar en este proyecto, <a href="mailto:enlacepilar@gmail.com">escribime.</a></p>
+    </div>
 </div>
 @endsection
--- resources/views/ayuda/proyecto.blade.php
+++ resources/views/ayuda/proyecto.blade.php
@@ -2,16 +2,18 @@
 @section('contenido_app')
 <div class="container mt-4">
     <img src="/imagenes/acerca-de-este-proyecto.png" class="img-fluid" alt="Acerca de este proyecto">
-    <p>Bienvenido a Biblioteca EnlaceLibre. Este proyecto surgió a partir de la materia "Culturas Digitales Libres" de la 
-    Universidad Nacional del Litoral, donde me encuentro realizado la Tecnicatura en Software Libre.</p>
-    <p>La idea es reunir en este sitio textos en formato PDF exclusivamente con licencias CC, es decir,
-    que se puedan descargar libremente y puedan ser, si asi se deseara por un lector, poder ser compartidos
-    o remixados sin restricciones. </p>
-    <p>Es por ello que las licencias tenidas en cuenta para subir al sitio serían, valga la 
-    restricción con <b>fines positivos</b>:</p>
-    <p><img src="https://www.creativecommons.org.ar/media/uploads/licencias/by-125px.png" alt="Licencia Atribucion"> <b>Atribución (by)</b>: Se permite cualquier explotación de la obra, incluyendo la explotación con fines comerciales y la creación de obras derivadas, la distribución de las cuales también está permitida sin ninguna restricción. Esta licencia es una licencia libre según la Freedom Defined.</p>
-    <p><img src="https://www.creativecommons.org.ar/media/uploads/licencias/by-sa-125px.png" alt="Licencia Reconocimiento"><b>Reconocimiento – Compartir Igual (by-sa)</b>: Se permite el uso comercial de la obra y de las posibles obras derivadas, la distribución de las cuales se debe hacer con una licencia igual a la que regula la obra original. Esta licencia es una licencia libre según la Freedom Defined.</p>
+    <div class="alert-warning p-4">
+        <p>Bienvenido a Biblioteca EnlaceLibre. Este proyecto surgió a partir de la materia "Culturas Digitales Libres" de la 
+        Universidad Nacional del Litoral, donde me encuentro realizado la Tecnicatura en Software Libre.</p>
+        <p>La idea es reunir en este sitio textos en formato PDF exclusivamente con licencias CC, es decir,
+        que se puedan descargar libremente y puedan ser, si asi se deseara por un lector, poder ser compartidos
+        o remixados sin restricciones. </p>
+        <p>Es por ello que las licencias tenidas en cuenta para subir al sitio serían, valga la 
+        restricción con <b>fines positivos (entiéndase fines positivos como compartirlo todo, la base para una cultura libre)</b>:</p>
+        <p><img src="https://www.creativecommons.org.ar/media/uploads/licencias/by-125px.png" alt="Licencia Atribucion"> <b>Atribución (by)</b>: Se permite cualquier explotación de la obra, incluyendo la explotación con fines comerciales y la creación de obras derivadas, la distribución de las cuales también está permitida sin ninguna restricción. Esta licencia es una licencia libre según la Freedom Defined.</p>
+        <p><img src="https://www.creativecommons.org.ar/media/uploads/licencias/by-sa-125px.png" alt="Licencia Reconocimiento"><b>Reconocimiento – Compartir Igual (by-sa)</b>: Se permite el uso comercial de la obra y de las posibles obras derivadas, la distribución de las cuales se debe hacer con una licencia igual a la que regula la obra original. Esta licencia es una licencia libre según la Freedom Defined.</p>
 
-    <a href="https://creativecommons.org.ar/licencias/" target="_blank">Más info sobre las licencias Creative Commons</a>
+        <a href="https://creativecommons.org.ar/licencias/" target="_blank">Más info sobre las licencias Creative Commons</a>
+    </div>
 </div>
 @endsection
